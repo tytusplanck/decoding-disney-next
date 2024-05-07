c0b27b535920679efbb80fbb7f1f387239b5890f
Tytus Planck <tytusplanck@gmail.com>
2024-05-06 20:53:31 -0400

    Post header clean

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -11,11 +11,13 @@ type Props = {
 export function PostHeader({ title, coverImage, date }: Props) {
   return (
     <>
-      <PostTitle>{title}</PostTitle>
       <div className="mb-8 md:mb-16 sm:mx-0 flex justify-center">
         <CoverImage title={title} src={coverImage} />
       </div>
-      <div className="max-w-2xl mx-auto">
+      <div className="flex justify-center">
+        <PostTitle>{title}</PostTitle>
+      </div>
+      <div className="flex justify-center max-w-2xl mx-auto">
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
